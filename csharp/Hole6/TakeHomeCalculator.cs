640e10218232ddfbf28f6af9f1b60be2f20b5183
AmadorAidaCanarias <amlopez@aidacanarias.com>
2022-05-18 15:35:57 +0100

    resharper: convert for to linq resharper: inline variable

--- csharp/Hole6/TakeHomeCalculator.cs
+++ csharp/Hole6/TakeHomeCalculator.cs
@@ -1,28 +1,16 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
 
-namespace Hole6
-{
-    public class TakeHomeCalculator
-    {
+namespace Hole6 {
+    public class TakeHomeCalculator {
         private readonly TaxRate taxRate;
 
-        public TakeHomeCalculator(TaxRate taxRate)
-        {
+        public TakeHomeCalculator(TaxRate taxRate) {
             this.taxRate = taxRate;
         }
 
-        public Money NetAmount(Money first, params Money[] rest)
-        {
-            List<Money> monies = rest.ToList();
-
-            Money total = first;
-
-            foreach (Money next in monies)
-            {
-                total = total.Plus(next);
-            }
-
+        public Money NetAmount(Money first, params Money[] rest) {
+            Money total = rest.ToList().Aggregate(first, (current, next) => current.Plus(next));
             Money tax = taxRate.Apply(total);
             return total.Minus(tax);
         }
